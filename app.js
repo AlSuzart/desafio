2555560be75d9709c3379376ce91abd95a6dec9b
AlSuzart <soijoh@hotmail.com>
2024-11-24 14:42:24 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,12 @@
+listaGenerica = [];
+console.log(listaGenerica);
+
+linguagensDeProgramacao = ['JS', 'C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+
+console.log(linguagensDeProgramacao[0]);
+
+
